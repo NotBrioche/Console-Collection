cfdc961eed71effb07598dbdb7c60f893b7f9769
NotBrioche <fabian.famille08@gmail.com>
2025-03-21 19:29:34 +0100

    ➕Added weather items

--- src/condition.ts
+++ src/condition.ts
@@ -3,12 +3,23 @@ class Condition {
   time: string;
   season: string;
   item: number;
+  moon: string;
+  near: string[];
 
-  constructor(action: string, time: string, season: string, item: number) {
+  constructor(
+    action: string,
+    time: string,
+    season: string,
+    item: number,
+    moon: string,
+    near: string[]
+  ) {
     this.action = action;
     this.time = time;
     this.season = season;
     this.item = item;
+    this.moon = moon;
+    this.near = near;
   }
 }
 
