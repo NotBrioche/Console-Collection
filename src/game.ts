904cb31271e87f5cb21ef945ee01665df666ddde
NotBrioche <fabian.famille08@gmail.com>
2025-03-21 13:19:45 +0100

    ➕User data file
    - A JSON file is created for the user infos, a username is prompted for the player if no fle exists
    - Added configuration for debug

--- src/game.ts
+++ src/game.ts
@@ -1,17 +1,44 @@
+import * as readline from 'readline/promises';
 import Player from './player';
 import Console from './console';
 import { exit } from 'process';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const rl = readline.createInterface(process.stdin, process.stdout);
 
 class Game {
-  player: Player = new Player('Fabrioche');
-  console: Console = new Console(this);
+  player: Player;
+  console: Console;
+
+  static playerDataPath: string = path.join(__dirname, '../data/player.json');
 
   constructor() {
+    if (fs.existsSync(Game.playerDataPath)) {
+      const fileString = fs.readFileSync(Game.playerDataPath, 'utf-8');
+
+      const importedPlayer = JSON.parse(fileString);
+
+      this.player = new Player(importedPlayer.username);
+    } else {
+      this.player = new Player('');
+    }
+
+    this.console = new Console(this);
+
     this.init();
   }
 
-  init() {
-    this.console.init();
+  async init() {
+    if (this.player.username == '') {
+      this.player.username = await rl.question(
+        "Quel est votre nom d'utilisateur ? : "
+      );
+      fs.writeFileSync(Game.playerDataPath, JSON.stringify(this.player), {
+        flag: 'w',
+      });
+    }
+    await this.console.init();
     exit();
   }
 }
--- tests/item.test.ts
+++ tests/item.test.ts
@@ -1,49 +1,93 @@
-import Item from "../src/item";
+import Item from '../src/item';
 
 describe('Item class', () => {
   test('An object created with the constructor has the right values', () => {
-    const item = new Item("Pierre", "C'est une pierre, elle peut vous porter compagnie", Math.random(), [], "Matériaux", 0, true);
+    const item = new Item(
+      1,
+      'Pierre',
+      "C'est une pierre, elle peut vous porter compagnie",
+      Math.random(),
+      [],
+      'Matériaux',
+      0,
+      true
+    );
 
-    expect(item.name).toBe("Pierre");
-    expect(item.description).toBe("C'est une pierre, elle peut vous porter compagnie");
+    expect(item.name).toBe('Pierre');
+    expect(item.description).toBe(
+      "C'est une pierre, elle peut vous porter compagnie"
+    );
     expect(item.quality).toBeGreaterThan(0);
     expect(item.quality).toBeLessThan(1);
     expect(item.rareVariant).toBe(true);
-    expect(item.collection).toBe("Matériaux");
-    expect(item.rarity).toBe("Common");
+    expect(item.collection).toBe('Matériaux');
+    expect(item.rarity).toBe('Common');
     expect(item._rarity).toBe(0);
   });
 
-  for (let i = 0; i < Item.rarity.length; i++) { 
+  for (let i = 0; i < Item.rarity.length; i++) {
     test(`An object created with the constructor has the right rarity ${Item.rarity[i]}`, () => {
-      const item = new Item("Pierre", "C'est une pierre, elle peut vous porter compagnie", Math.random(), [], "Matériaux", i);
-  
+      const item = new Item(
+        1,
+        'Pierre',
+        "C'est une pierre, elle peut vous porter compagnie",
+        Math.random(),
+        [],
+        'Matériaux',
+        i
+      );
+
       expect(item.rarity).toBe(Item.rarity[i]);
-    })
+    });
   }
 
   test('An object created with the constructor has the right rarity if the value is null', () => {
-    const item = new Item("Pierre", "C'est une pierre, elle peut vous porter compagnie", Math.random(), [], "Matériaux", null);
+    const item = new Item(
+      1,
+      'Pierre',
+      "C'est une pierre, elle peut vous porter compagnie",
+      Math.random(),
+      [],
+      'Matériaux',
+      null
+    );
 
-    expect(item.rarity).toBe("Common");
+    expect(item.rarity).toBe('Common');
     expect(item._rarity).toBe(0);
-  })
+  });
 
   test('An object created with the constructor throws an error if the value is out of bounds', () => {
     const act = () => {
-      new Item("Pierre", "C'est une pierre, elle peut vous porter compagnie", Math.random(), [], "Matériaux", 6);
+      new Item(
+        1,
+        'Pierre',
+        "C'est une pierre, elle peut vous porter compagnie",
+        Math.random(),
+        [],
+        'Matériaux',
+        6
+      );
     };
 
-    expect(act).toThrow(`Rarity must be between 0 and ${Item.rarity.length - 1}`);
-  }
-  );
+    expect(act).toThrow(
+      `Rarity must be between 0 and ${Item.rarity.length - 1}`
+    );
+  });
 
-  test('details of an item calls the getObjectInfos method', () => { 
-    const item = new Item("Pierre", "C'est une pierre, elle peut vous porter compagnie", Math.random(), [], "Matériaux", 0);
+  test('details of an item calls the getObjectInfos method', () => {
+    const item = new Item(
+      1,
+      'Pierre',
+      "C'est une pierre, elle peut vous porter compagnie",
+      Math.random(),
+      [],
+      'Matériaux',
+      0
+    );
 
-    const spy = jest.spyOn(item, "getObjectInfos");
+    const spy = jest.spyOn(item, 'getObjectInfos');
     item.details;
 
     expect(spy).toHaveBeenCalled();
-  })
+  });
 });
